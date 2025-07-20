eb89d30ab353b7eb96eb691097d8241f9af2ac19
leonard towtt <40164766+kipngetichi@users.noreply.github.com>
2025-07-20 20:27:16 +0300

    update changes learned

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>this is my first app!<br/>
+        this is my first app!<br/>
+        this is my first app!
+      </Text>
+      <Text>enjoy react!<br/></Text>
+      
       <StatusBar style="auto" />
+      <Button  title="press me" />
     </View>
   );
 }
